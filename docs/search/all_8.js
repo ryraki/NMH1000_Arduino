e1fad1a53873a6f6b1200ef7f49fea484b1d4479
Tedd OKANO <okano.akifumi@gmail.com>
2023-07-10 11:06:21 +0900

    document fix and re-generate

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['read_0',['read',['../class_n_m_h1000___arduino.html#abf371f3336bccc475f00e9e1a906610f',1,'NMH1000_Arduino::read()'],['../class_n_m_h1000___arduino.html#a138c310ad574747147b02a749739530e',1,'NMH1000_Arduino::read(void)']]],
+  ['read_0',['read',['../class_n_m_h1000___arduino.html#abf371f3336bccc475f00e9e1a906610f',1,'NMH1000_Arduino']]],
   ['read_5fr16_1',['read_r16',['../class_n_m_h1000___arduino.html#a3bdf57898e29f42ead2392defaa30857',1,'NMH1000_Arduino']]],
   ['read_5fr8_2',['read_r8',['../class_n_m_h1000___arduino.html#aeb6915b7ee1176b154fa3558e85acbc7',1,'NMH1000_Arduino']]],
   ['reg_5fr_3',['reg_r',['../class_n_m_h1000___arduino.html#ac0bfa4b931c5098e7316938fb32021c6',1,'NMH1000_Arduino::reg_r(uint8_t reg_adr, uint8_t *data, uint16_t size)'],['../class_n_m_h1000___arduino.html#aa99f0ec64fd72f85b2f9dae0fd8ca7f3',1,'NMH1000_Arduino::reg_r(uint8_t reg_adr)']]],
